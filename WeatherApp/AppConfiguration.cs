87ffb1ca3ef395cbe77fdc68f74da2b698325a75
Bloodinix <williamwillow3@gmail.com>
2021-11-15 04:34:43 -0500

    AppConfiguration complété - 1769746

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class AppConfiguration
+    {
+        private static IConfiguration configuration;
+        public static string GetValue(string key)
+        {
+            if (configuration == null)
+                initConfig();
+
+            return configuration[key];
+        }
+
+        private static void initConfig()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddJsonFile("appsettings.json",
+                optional: true,
+                reloadOnChange: true);
+
+            builder.AddUserSecrets<MainWindow>();
+
+            configuration = builder.Build();
+        }
+    }
+}
